82901d60821b477a7da83de03c268c99da5b9d2b
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 13:10:39 -0600

    Vehicle

--- ConsoleUI/Vehicle.cs
+++ ConsoleUI/Vehicle.cs
@@ -4,14 +4,24 @@ using System.Text;
 
 namespace ConsoleUI
 {
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
-    class Vehicle
+    /*
+     * Create an abstract class called Vehicle
+     * The vehicle class shall have three string properties Year, Make, and Model
+     * Set the defaults to something generic in the Vehicle class
+     * Vehicle shall have an abstract method called DriveAbstract with no implementation
+     * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+     */
+    abstract class Vehicle
+    {
+        public int Year { get; set; } = 0000
+        public string Make { get; set; } = "Generic Make"
+        public string Model { get; set; } = "Generic Model"
+    }
+    public abstract void DriveAbstract();
+
+    public virtual void DriveAbstract();
     {
     }
+
+
 }
