f01ed3c03b5022389b0dba63165eb16d263d0e64
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 13:55:58 -0600

    VehiclesCreated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace ConsoleUI
 {
@@ -13,13 +12,17 @@ namespace ConsoleUI
         {
 
             // Create a list of Vehicle called vehicles
-            Vehicle Vehicles = new List<>();
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car classic = new Car() { HasTrunk = true, Make = "Chevrolet", Model = "Classic", Year = 2004 };
+            Motorcycle harley = new Motorcycle() { HasSideCart = true, Make = "Harley Davidson", Model = "XR750", Year = 2015 };
+            Vehicle golfCart = new Car() { HasTrunk = true, Make = "Yamaha", Model = "Drive", Year = 2009 };
+            Vehicle goKart = new Car() { HasTrunk = false, Make = "Kandy", Model = "150cc", Year = 2020 };
 
             /*
              * Add the 4 vehicles to the list
@@ -27,7 +30,7 @@ namespace ConsoleUI
              */
 
             // Call each of the drive methods for one car and one motorcycle
-         
+
             Console.ReadLine();
         }
     }
