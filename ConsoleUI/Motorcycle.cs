9f67f21ce4bc287b4399e2033e60b0f7204296bb
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 13:40:17 -0600

    MotorcycleDone

--- ConsoleUI/Motorcycle.cs
+++ ConsoleUI/Motorcycle.cs
@@ -4,25 +4,28 @@ using System.Text;
 
 namespace ConsoleUI
 {
-           /* 
-            * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-            * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-            * Provide the implementations for the abstract methods
-            * Only in the Motorcycle class will you override the virtual drive method
-           */
-         
+    /* 
+     * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+     * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+     * Provide the implementations for the abstract methods
+     * Only in the Motorcycle class will you override the virtual drive method
+    */
+
     public class Motorcycle : Vehicle
     {
-        public Motorcyle()
+        public Motorcycle()
         {
-
         }
         public bool HasSideCart { get; set; }
-    
 
-    public abstract void DriveAbstract();
-
-    public virtual void DriveAbstract();
-    {
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive.");
+        }
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} that virtually in drive");
+        }
     }
+    
 }
